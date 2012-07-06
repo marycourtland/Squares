3fb4837938d401edc9d0d3332166b337db38fe41
Mary <marycbush@gmail.com>
2012-07-05 18:08:22 -0700

    Changed button placement; fixed mouse.js to work for both Firefox and Chrome.

--- akihabara/mouse.js
+++ akihabara/mouse.js
@@ -30,13 +30,23 @@ mouse = gbox.addObject({
     },
     
     move: function(event) {
-    var tempCanvas = document.getElementsByTagName("CANVAS")[0];
-    var cam = gbox.getCamera();
-    //mouse.x = (event.layerX - tempCanvas.offsetLeft)/gbox._zoom + cam.x;
-    //mouse.y = (event.layerY - tempCanvas.offsetTop)/gbox._zoom + cam.y;
-    mouse.x = event.layerX/gbox._zoom + cam.x;
-    mouse.y = event.layerY /gbox._zoom + cam.y;
-    },
+		var tempCanvas = document.getElementsByTagName("CANVAS")[0];
+		var cam = gbox.getCamera();
+		
+		if (navigator.userAgent.match(/Firefox/i)) {
+			mouse.x = (event.layerX - tempCanvas.offsetLeft)/gbox._zoom + cam.x;
+			mouse.y = (event.layerY - tempCanvas.offsetTop)/gbox._zoom + cam.y;
+		}
+		else if (navigator.userAgent.match(/Chrome/i)) {
+			mouse.x = event.layerX/gbox._zoom + cam.x;
+			mouse.y = event.layerY/gbox._zoom + cam.y;
+		}
+		else {
+			// This is the same as the chrome code
+			mouse.x = event.layerX/gbox._zoom + cam.x;
+			mouse.y = event.layerY/gbox._zoom + cam.y;
+		}
+	},
     
     down: function(event) {
     mouse.isDown = true;
